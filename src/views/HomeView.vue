f3ca68215d8f5929932159c51066d588d97ab57f
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-02-14 14:21:12 -0300

    💄 Organização visual do projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,46 @@
+<template>
+  <div class="home">
+    <div class="count">
+      {{ count }}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
+    <hr>
+    <div>
+      This counter is: {{ oddOrEven }}
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, computed } from 'vue';
+
+const count = ref(0);
+
+const increaseCount = () => {
+  count.value++;
+}
+
+const decreaseCount = () => {
+  count.value--;
+}
+
+const oddOrEven = computed(() => {
+  if(count.value % 2 === 0) return 'even';
+  return 'odd';  
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px
+}
+
+.buttons button {
+  font-size: 40px;
+  margin: 10px
+}
+</style>
